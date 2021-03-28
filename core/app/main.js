cd3a2dc3b3b5be5f5ecb54822f625d48d7672fa3
siddharth <siddharth.ashutosh7@gmail.com>
2021-03-28 22:55:00 +0530

    added webserver

---
+++ core/app/main.js
@@ -0,0 +1,36 @@
+const app = new PIXI.Application({
+    width: 720, height: 360, backgroundColor: 0x000000, resolution: window.devicePixelRatio || 1,
+});
+document.body.appendChild(app.view);
+
+const container = new PIXI.Container();
+
+app.stage.addChild(container);
+
+// Create a new texture
+const texture = PIXI.Texture.from('../games/game1/dist/mario.png');
+
+// Create a 5x5 grid of bunnies
+for (let i = 0; i < 25; i++) {
+    const bunny = new PIXI.Sprite(texture);
+    bunny.anchor.set(0.5);
+    bunny.scale.set(0.2);
+    bunny.x = (i % 5) * 40;
+    bunny.y = Math.floor(i / 5) * 40;
+    container.addChild(bunny);
+}
+
+// Move container to the center
+container.x = app.screen.width / 2;
+container.y = app.screen.height / 2;
+
+// Center bunny sprite in local container coordinates
+container.pivot.x = container.width / 2;
+container.pivot.y = container.height / 2;
+
+// Listen for animate update
+app.ticker.add((delta) => {
+    // rotate the container!
+    // use delta to create frame-independent transform
+    container.rotation -= 0.01 * delta;
+});
--- webserver.js
+++ webserver.js
@@ -1,6 +1,6 @@
 const path = require('path');//check these and in script start (pacakge.json) and how its happening when i am launching local host
-const express = require('express');
-const app = express();
+const express = require('express'); // why in local thats not happening?
+const app = express(); //in server is package.json also deployed?if no how its starting?
 const port = 2000;
 
 const gamePath = path.join(__dirname,'./');
@@ -10,3 +10,12 @@ app.use(express.static(gamePath)); //One way : takes current directory by gamepa
 app.listen(port, function(){
     console.log("web server is running with port " + port);
 })
+// app.get('/about1', function(req, res){ 
+//     res.send('hello'); // **IMPORTANT!!!** if u typle manually from server then send data in page u will see else if from HTML js then in console
+// })
+// const http = require('http');
+// const server = http.createServer(function(request,response){
+//     response.write('hello');
+//     response.end();
+// });
+// server.listen(2000);
